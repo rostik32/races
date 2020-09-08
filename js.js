6198001a90abc7a2dbf8120d19d9cb34787e2707
rostik32 <62945820+rostik32@users.noreply.github.com>
2020-09-08 15:37:49 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,49 @@
+const score = document.querySelector('.score');
+const start = document.querySelector('.start');
+const gameArea = document.querySelector('.gameArea');
+const car = document.createElement('div');
+car.classList.add('car');
+
+
+start.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
+
+const keys = {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowRight: false,
+    ArrowLeft: false,
+};
+
+const setting = {
+    start: false,
+    score: 0,
+    speed: 3,
+};
+
+function startGame() {
+    start.classList.add('hide');
+    setting.start = true;
+    gameArea.appendChild(car);
+    if (setting.start) {
+        requestAnimationFrame(playGame);
+    }
+}
+
+function playGame() {
+    console.log('Play game!');
+    requestAnimationFrame(playGame);
+}
+
+function startRun(event) {
+    event.preventDefault();
+    console.log(event.key);
+    keys[event.key] = true;
+}
+
+function stopRun(event) {
+    event.preventDefault();
+    console.log(event.key);
+    keys[event.key] = false;
+}
